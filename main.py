5025825345115c241f1054fd35bee3051cd54341
Ashutosh45-maker <ashutoshparadkar05@gmail.com>
2024-10-05 21:23:53 +0530

    data types and operators

---
+++ main.py
@@ -0,0 +1,35 @@
+name="Ashutosh"
+age=10
+grade=11
+data=True
+
+print("my name is",name)
+print (type(name))
+print("my age is",age)
+print(type(age))
+print(type(data))
+
+#activity 2
+num1=45
+num2=75
+
+print("addition",num1+num2)
+print("difference",num1-num2)
+print("product",num1*num2)
+print("division",num1/num2)
+print("remainder",num1%num2)
+print("square of a number",num1**2)
+print("equal?",num1==num2)
+print("greater than?",num1>num2)
+print("less than",num1<num2)
+
+
+#activity 3
+fname="Cricket council"
+lname="India"
+
+fullname=fname+" "+lname
+print(fullname)
+print(len(fullname))
+print(fname[5])
+
